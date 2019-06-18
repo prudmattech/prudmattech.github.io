4d56ad57be4933485d9a2f95c6f1dab8bfddc15f
Prudmat Technologies <prudmattech@gmail.com>
2019-06-18 18:55:15 +0530

    Prudmat Tech IDCheck

---
+++ idscan/precache-manifest.e2eaf0c14642a82cfa8a87e561236e9a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9419f2a8f929878704774649867d3baa",
+    "url": "https://prudmattech.github.io/idscan/index.html"
+  },
+  {
+    "revision": "bd13a3a2ed17ab2808d3",
+    "url": "https://prudmattech.github.io/idscan/static/css/main.46624fe2.chunk.css"
+  },
+  {
+    "revision": "e803eae092b3a5206533",
+    "url": "https://prudmattech.github.io/idscan/static/js/2.5ef2cd62.chunk.js"
+  },
+  {
+    "revision": "bd13a3a2ed17ab2808d3",
+    "url": "https://prudmattech.github.io/idscan/static/js/main.1cc67321.chunk.js"
+  },
+  {
+    "revision": "aad8df7650fd1de66943",
+    "url": "https://prudmattech.github.io/idscan/static/js/runtime~main.d74b69d0.js"
+  },
+  {
+    "revision": "0c7248855fa32cf64ed57349a6fb6df4",
+    "url": "https://prudmattech.github.io/idscan/static/media/card_back@3x.0c724885.png"
+  },
+  {
+    "revision": "d268a33dd7020770c9be64f14571759a",
+    "url": "https://prudmattech.github.io/idscan/static/media/illustration1@3x.d268a33d.png"
+  },
+  {
+    "revision": "406cb2b985fcbd5dca1111b268798326",
+    "url": "https://prudmattech.github.io/idscan/static/media/illustration2@3x.406cb2b9.png"
+  },
+  {
+    "revision": "5e4db5272f8a48d5505821f0fb762833",
+    "url": "https://prudmattech.github.io/idscan/static/media/image_correct.5e4db527.jpg"
+  },
+  {
+    "revision": "0b1f3e62b46ab75ae3ea73edf8f65063",
+    "url": "https://prudmattech.github.io/idscan/static/media/scan_process.0b1f3e62.gif"
+  }
+]);
